015417c3499294c343c65d8c6606030a8e2a6e97
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2017-10-28 23:24:33 -0400

    Add more messages

--- messages.js
+++ messages.js
@@ -18,13 +18,9 @@ let msgs = new localize({
     "EN": "Welcome! Congratulations on your pregnancy. This is a very exciting time for you – but you might have some questions. It seems you are in month two of your pregnancy. Pregnancy is divided into three parts, each three months long. Ammi service will send you weekly messages with tips, advice, and information so you and your baby are healthy! If you haven’t yet, please plan to go to a clinic soon or the LHW of your community",
     "UR": "week 5 msg 1 in urdu"
   },
-  "5-2-1": {
+  "5-2": {
     "EN": "You might want to keep your pregnancy a secret. But the best thing you can do is talk to a LHW. She will be your friend on your journey to motherhood; Regular clinic visits in pregnancy help detect problems before they happen. Find out where your nearest clinic is. Even though you are perfectly healthy, you should have at least four clinic visits during pregnancy to make sure you and your baby are well.  try to go once before your third month ends, once before your 6th month, and twice in the last three months.Talk to your family about why it is important for  you to go to regular clinic visits.",
-    "UR": "week 5 1 msg 2 a in urdu"
-  },
-  "5-2-2": {
-    "EN": "You might be feeling tired and nauseous. Most women do in early pregnancy. Try having some ginger, mint or lemon tea, and rest if you can.  Ask your LHW about Iron and Folic Acid tablets -- taking one every day it will help your baby grow well.",
-    "UR": "week 5 msg 2 b in urdu"
+    "UR": "week 5 1 msg 2 in urdu"
   },
   "6-1": {
     "EN": "Spotting or light bleeding is worrying but very common in pregnancy. Slight bleeding is very common in early pregnancy. You may worry that it's a sign of a miscarriage. But there can be many reasons for bleeding. Some women have bleeding when they would usually have their period. Or it may mean that your baby is attaching himself to your womb. Rest for a while if you can.",
@@ -33,6 +29,286 @@ let msgs = new localize({
   "6-2": {
     "EN": "Regular clinic visits in pregnancy help detect problems before they happen. Go to the clinic if the bleeding is heavy, or if it comes with stomach pain. You need to go to the clinic immediately if you cannot lift your arm above your shoulder or if there is a pain in your shoulder. If you are worried about your pregnancy, talk to your LHW She'll be happy to answer your questions.",
     "UR": "week 6 msg 2 in urdu"
+  },
+  "7-1": {
+    "EN": "Your baby's heart and brain are forming. Take iron and folic acid pills daily to help her stay safe. Eating well is very important now. Iron-rich foods such as meat, leafy green vegetables, lentils, beans, spinach and eggs. Getting enough iron will help you be less tired and your baby will thrive. Have some fresh fruit or juice with your meal to help your body absorb the iron.",
+    "UR": "week 7 msg 1 in urdu"
+  },
+  "7-2": {
+    "EN": "Eating well is very important now.Try to eat some meat, eggs, bean or lentils every day and green vegetables and milk, yoghurt or soya. Energy-giving foods such as rice, wheat, and oats.Body-building foods such as meat, eggs and lentils.Protective foods such as vegetables and fruits. These help reduce the risk of your baby being born with a problem.",
+    "UR": "week 7 msg 2 in urdu"
+  },
+  "8-1": {
+    "EN": "Eating well is very important now.Try to eat some meat, eggs, bean or lentils every day and green vegetables and milk, yoghurt or soya. Energy-giving foods such as rice, wheat, and oats.Body-building foods such as meat, eggs and lentils.Protective foods such as vegetables and fruits. These help reduce the risk of your baby being born with a problem.",
+    "UR": "week 8 msg 1 in urdu"
+  },
+  "8-2": {
+    "EN": "It will soon be time for your first clinic visit. You may need help from your family to get to the clinic, so talk to them now. Take some food and water with you to the clinic so you can eat something while you wait. You may want to ask a friend to come with you for company.",
+    "UR": "week 8 msg 2 in urdu"
+  },
+  "9-1": {
+    "EN": "congratulations! You have completed your first two months. Bad or stale food can make you ill. Make sure that all your food is fresh. Cook fresh meals every day. Store food in a cool, dry place.Cook eggs well, until they are firm. Boil milk to kill any germs.Do not eat food from stalls. It can have germs. Instead, take a snack and clean water with you when you go out.",
+    "UR": "week 9 msg 1 in urdu"
+  },
+  "9-2": {
+    "EN": "Protect yourself from infections. Wash your hands with soap before you prepare meals, after using the toilet and after handling animals.Wash all dishes and cooking areas with soapy water to get rid of any food remains.Keep animals away from the kitchen. Animals can spread germs and make you sick.",
+    "UR": "week 9 msg 2 in urdu"
+  },
+  "10-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "10-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "11-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "11-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "12-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "12-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "13-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "13-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "14-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "14-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "15-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "15-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "16-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "16-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "17-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "17-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "18-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "18-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "19-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "19-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "20-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "20-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "21-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "21-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "22-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "22-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "23-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "23-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "24-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "24-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "25-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "25-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "26-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "26-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "27-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "27-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "28-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "28-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "29-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "29-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "30-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "30-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "31-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "31-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "32-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "32-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "33-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "33-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "34-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "34-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "35-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "35-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "36-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "36-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "37-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "37-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "38-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "39-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "40-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "40-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "41-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "41-2": {
+    "EN": "",
+    "UR": ""
+  },
+  "42-1": {
+    "EN": "",
+    "UR": ""
+  },
+  "42-2": {
+    "EN": "",
+    "UR": ""
   }
 });
 //TODO: move all such calls to an 'init' method
